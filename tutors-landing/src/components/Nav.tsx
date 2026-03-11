f37ad6af0004c23f66d7a4ce336b7fce07e408ed
oljas <medetbaevoljas@gmail.com>
2026-03-11 21:49:46 +0100

    readme edited

--- tutors-landing/src/components/Nav.tsx
+++ tutors-landing/src/components/Nav.tsx
@@ -3,11 +3,7 @@ export default function Nav() {
     <nav className="nav">
       <ul className="nav__list flex">
         <li>
-<<<<<<< HEAD
           <a href="#myReviews">reviews</a>
-=======
-          <a href="#reviews">reviews</a>
->>>>>>> d4c01d48571c43f5b7ccac1206a8b65a26f1c6c1
         </li>
         <li>
           <a href="#inline-widget-meet-with-oljas-medetbaev">lessons</a>
